1bde6b69ab788e7f1b880f330fd1e7c910d2371a
KevinU20221275 <U20221275@univo.edu.sv>
2025-03-13 18:04:34 -0600

    document code

--- practica-3/Comida.js
+++ practica-3/Comida.js
@@ -1,36 +1,91 @@
+/**
+ * Description placeholder
+ *
+ * @class Comida
+ * @typedef {Comida}
+ */
 class Comida {
+    /**
+     * Creates an instance of Comida.
+     *
+     * @constructor
+     * @param {string} nombre 
+     * @param {string} tamaño 
+     * @param {number} precio 
+     */
     constructor(nombre, tamaño, precio){
         this.nombre = nombre;
         this.tamaño = tamaño;
         this.precio = precio;
     }
 
+    /**
+     * Description placeholder
+     *
+     * @returns {string} 
+     */
     informacion(){
         return `Nombre : ${this.nombre} \nTamaño: ${this.tamaño} \nPrecio: ${this.precio}`
     }
 }
 
 
+/**
+ * Description placeholder
+ *
+ * @type {Comida}
+ */
 let burrito = new Comida("Burrito", "Grande", 4.99)
 console.log(burrito.informacion())
 console.log("\n")
 
+/**
+ * Description placeholder
+ *
+ * @type {Comida}
+ */
 let pizza = new Comida("Pizza", "Mediana", 7.99)
 console.log(pizza.informacion())
 console.log("\n")
 
 
+/**
+ * Description placeholder
+ *
+ * @class Bebida
+ * @typedef {Bebida}
+ * @extends {Comida}
+ */
 class Bebida extends Comida{
+    /**
+     * Creates an instance of Bebida.
+     *
+     * @constructor
+     * @param {string} nombre 
+     * @param {string} tamaño 
+     * @param {number} precio 
+     * @param {boolean} refil 
+     */
     constructor(nombre, tamaño, precio, refil){
         super(nombre, tamaño, precio)
         this.refil = refil;
     }
 
+    /**
+    * Description placeholder
+    *
+    * @returns {string} 
+    */
     informacion(){
         return `${super.informacion()} \nRefil: ${this.refil ? 'Si' : 'No'}`
     }
 }
 
+/**
+ * Description placeholder
+ *
+ * @type {Bebida}
+ */
 let liquado = new Bebida("Liquado de Fresa", "XL", 1.25, false)
 console.log(liquado.informacion())
 console.log("\n")
@@ -42,10 +97,20 @@ class Entrada extends Comida {
         this.sabor = sabor;
     }
 
+    /**
+    * Description placeholder
+    *
+    * @returns {string} 
+    */
     informacion(){
         return `${super.informacion()} \nSabor: ${this.sabor}`
     }
 }
 
+/**
+ * Description placeholder
+ *
+ * @type {Entrada}
+ */
 let nachos = new Entrada("Nachos", "Pequeño", 2.5, "Picante")
 console.log(nachos.informacion())
--- practica-3/Vehiculo.js
+++ practica-3/Vehiculo.js
@@ -1,4 +1,19 @@
+
+/**
+ * Description placeholder
+ *
+ * @class MedioTrasporte
+ * @typedef {MedioTrasporte}
+ */
 class MedioTrasporte {
+    /**
+     * Creates an instance of MedioTrasporte.
+     *
+     * @constructor
+     * @param {string} marca 
+     * @param {string} modelo 
+     * @param {int} año 
+     */
     constructor(marca, modelo, año){
         this.marca = marca;
         this.modelo = modelo;
@@ -6,21 +21,52 @@ class MedioTrasporte {
         this.encendido = false;
     }
 
+    /**
+     * Description placeholder
+     *
+     * @returns {string} 
+     */
     caracteristicas(){
         return `Marca: ${this.marca} \nModelo: ${this.modelo} \nAño: ${this.año}`
     }
 }
 
+/**
+ * Description placeholder
+ *
+ * @class Moto
+ * @typedef {Moto}
+ * @extends {MedioTrasporte}
+ */
 class Moto extends MedioTrasporte {
+    /**
+     * Creates an instance of Moto.
+     *
+     * @constructor
+     * @param {string} marca 
+     * @param {string} modelo 
+     * @param {int} año 
+     * @param {number} cilindrada 
+     */
     constructor(marca, modelo, año, cilindrada){
         super(marca, modelo, año);
         this.cilindrada = cilindrada;
     }
 
+    /**
+    * Description placeholder
+    *
+    * @returns {string} 
+    */
     caracteristicas(){
         return `${super.caracteristicas()} \nCilindrada: ${this.cilindrada} cc`
     }
 }
 
+/**
+ * Description placeholder
+ *
+ * @type {Moto}
+ */
 let honda = new Moto("Honda", "Navi", 2025, 1.0)
 console.log(honda.caracteristicas())
